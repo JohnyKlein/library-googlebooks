f90ea3e821bb0c7a0b48f9741cbc6661c5d2af8f
Johny Klein <johnyklein@hotmail.com>
2020-02-05 00:01:57 -0300

    Obtendo nomes de livros pela request

--- library/src/main/java/com/library/app/LibraryController.java
+++ library/src/main/java/com/library/app/LibraryController.java
@@ -7,22 +7,42 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 
 @Controller
 public class LibraryController {
 
     @GetMapping(value="/")
-    public String test() {
+    public String login() {
         return "login";
     }
 
     @GetMapping("/books")
     public String listBooks(@RequestParam(name="nameSearch") String nameSearch, Model model) {
         String googleAddress = "https://www.googleapis.com/books/v1/volumes?q=" + nameSearch;
+        JSONObject response = getResponse();
+        List<String> titles = getTitles(response);
         model.addAttribute("nameSearch", nameSearch);
-        model.addAttribute("googleAPI", getResponse());
+        model.addAttribute("titles", titles);
+//        model.addAttribute("googleAPI", response);
 
         return "books";
+    }
+
+    private List<String> getTitles(JSONObject response) {
+        List<String> titles = new ArrayList<>();
+        List<HashMap<String, Object>> items = (List<HashMap<String, Object>>) response.toMap().get("items");
+
+        items.forEach(item -> {
+            HashMap<String, Object> volume = (HashMap<String, Object>) item.get("volumeInfo");
+            String title = (String) volume.get("title");
+            titles.add(title);
+        });
+
+        return titles;
     }
 
     private JSONObject getResponse() {
